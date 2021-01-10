ec1bb02e46c6795f108d1e5ab19054e116c5faf1
Praveen Kumar <praveenscience@gmail.com>
2021-01-10 20:36:49 +0000

    Reorganised the files inside src.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Welcome to Stuy Hacks!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
