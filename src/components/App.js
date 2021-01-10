767593c79ee8e7bffe904469351550ccf7dc7d9c
Praveen Kumar <praveenscience@gmail.com>
2021-01-10 20:53:22 +0000

    Toggling Dark and Light Mode using Event Handler.

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,30 @@ import React, { Component } from "react";
 import Header from "./Header";
 
 class App extends Component {
+  state = {
+    Dark: true
+  };
+  handleToggle = e => {
+    e.preventDefault();
+    this.setState({
+      Dark: !this.state.Dark
+    });
+  };
   render() {
     return (
       <div className="App">
-        <Header dark={true} className="mb-3">
+        <Header dark={this.state.Dark} className="mb-3">
           Stuy Hacks!
         </Header>
-        <p>Welcome to Stuy Hacks!</p>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <button className="btn btn-primary" onClick={this.handleToggle}>
+                Toggle
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
