92059285ab77601725afbe63ea84c4eb2e8e859f
Praveen Kumar <praveenscience@gmail.com>
2021-01-10 20:49:21 +0000

    Added a dark header in the App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header dark={true} className="mb-3">
+          Stuy Hacks!
+        </Header>
         <p>Welcome to Stuy Hacks!</p>
       </div>
     );
